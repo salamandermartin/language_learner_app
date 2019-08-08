ca278bee36d6f402f7f7b733105d26070dca23df
Martin Salamanca <salamancam.programming@gmail.com>
2019-08-08 10:08:15 -0400

    testing the welcome page, commented unfinished code out

--- main.py
+++ main.py
@@ -39,6 +39,6 @@ class WelcomePageHandler(webapp2.RequestHandler):
 #app configuration section
 app = webapp2.WSGIApplication([
     ('/', WelcomePageHandler) #Welcome Page
-    ('/list-go', ListPageHandler) #List Page
-    ('/result', ResultPageHandler) #Results Page
+    #('/list-go', ListPageHandler) #List Page
+    #('/result', ResultPageHandler) #Results Page
 ], debug=True)
